f8c5a5c1a88575c70ba503f56ae5f24cc8a7e12a
JacksonM1456 <124176773+JacksonM1456@users.noreply.github.com>
2023-02-01 08:51:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ p = 0
 
 import turtle
 t=turtle.Turtle()
-t.color("black", "green")
+t.color("black", "yellow")
 t.begin_fill()
 t.right(90)
 t.forward(100)
@@ -18,18 +18,25 @@ t.left(90)
 t.forward(300)
 t.end_fill()
 
-
+#Head
+t.begin_fill()
+t.color("black", "purple")
 while p<4:
     p = p+1
     t.right(90)
     t.forward(75)
-t.color("black", "purple")
+    t.color("black", "red")
+t.end_fill()
+
+#Sunglasses
 t.begin_fill
 t.backward(15)
 t.right(90)
 t.penup()
 t.forward(50)
 t.pendown()
+t.color("black", "black")
+t.begin_fill()
 t.right(90)
 t.forward(60)
 t.right(90)
@@ -42,5 +49,41 @@ t.right(90)
 t.forward(25)
 t.right(90)
 t.forward(15)
+t.left(90)
+t.forward(10)
+t.left(90)
+t.forward(15)
+t.right(90)
+t.forward(25)
+t.right(90)
+t.forward(25)
+t.right(90)
+t.forward(75)
+t.end_fill()
 
+#Right horn
+t.left(90)
+t.forward(25)
+t.color("black", "orange")
+t.begin_fill()
+t.right(35)
+t.forward(20)
+t.left(165)
+t.forward(25)
+t.end_fill()
 
+#left horn
+t.right(40)
+t.forward(65)
+t.color("black", "orange")
+t.begin_fill()
+t.right(35)
+t.forward(20)
+t.right(165)
+t.forward(35)
+t.end_fill()
+t.left(20)
+t.forward(60)
+
+t.right(90)
+t.foward(70)
