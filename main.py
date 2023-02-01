e2c341e32fdc86d0886be26e4e9303b9265cddfc
hydradict <124175937+hydradict@users.noreply.github.com>
2023-02-01 08:14:41 -0700

    Update and rename README.md to main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+# dragon.py
+
+p = 0
+
+
+
+import turtle
+t=turtle.Turtle()
+t.color("black", "green")
+t.begin_fill()
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(250)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(300)
+t.end_fill()
+
+
+while p<4:
+    p = p+1
+    t.right(90)
+    t.forward(75)
+t.color("black", "purple")
+t.begin_fill
+t.backward(15)
+t.right(90)
+t.penup()
+t.forward(50)
+t.pendown()
+t.right(90)
+t.forward(60)
+t.right(90)
+t.forward(10)
+t.right(90)
+t.forward(15)
+t.left(90)
+t.forward(15)
+t.right(90)
+t.forward(25)
+t.right(90)
+t.forward(15)
+
+
